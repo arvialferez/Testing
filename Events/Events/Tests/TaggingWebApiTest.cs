2ea5d059096b9b408bfe1bb2ebfd710c2e0010fc
Arvi Alferez <alferez.arvi@gmail.com>
2015-11-21 00:50:50 -0800

    Rest

--- Events/Events/Tests/TaggingWebApiTest.cs
+++ Events/Events/Tests/TaggingWebApiTest.cs
@@ -7,7 +7,7 @@ namespace Events.Tests
     [TestClass]
     public class TaggingWebApiTest : Test
     {
-        [TestMethod]
+        //[TestMethod]
         public void BrowserTest()
         {
             driverGC.Navigate().GoToUrl("http://www.google.com");
@@ -15,5 +15,35 @@ namespace Events.Tests
             driverGC.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
             driverGC.Quit();
         }
+
+        [TestMethod]
+        public void WebApiPendingTest()
+        {
+            CreateAssignEIDEvent();
+
+            // Verify the event in AnimalEvent table with a status of pending
+
+            // Verify the event in AI proposed assign eid table
+
+        }
+
+        public string CreateAssignEIDEvent()
+        {
+            // Create the body to be used for the request
+            string body = string.Empty;
+
+            // Create an authentication token
+
+            // Send the request
+
+            return null;
+        }
+
+        public void Send()
+        {
+ 
+        }
+
     }
+
 }
---
+++ Events/Events/WebAPI/Request.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.WebAPI
+{
+    class Request:RestRequest
+    {
+        public DateTimeOffset SentAt { get; set; }
+
+        public Uri AbsoluteUrl { get; private set; }
+        public string BaseUrl { get; private set; }
+        public string ResourcePath { get; private set; }
+
+        public Request(Uri absoluteUrl, string baseUrl, string resourcePath, Method method) : base(resourcePath, method)
+		{
+
+            var url = GetUri();
+            var baseUrl = GetBaseUrl(url);
+            var resourcePath = GetResourcePath(url);
+
+            foreach (var header in Headers.Where(h => h.Name != Header.ContentType))
+                request.AddHeader(header.Name, header.Value);
+
+            request.AddParameter(Headers.Single(h => h.Name == Header.ContentType).Value, Body, ParameterType.RequestBody);
+
+
+
+            AbsoluteUrl = absoluteUrl;
+            BaseUrl = baseUrl;
+            ResourcePath = resourcePath;
+        }
+    }
+}
